a62838b974e73f37cce6d051f3811fbb8ff3dd62
nik353198 <nik353198@gmail.com>
2019-03-28 20:04:57 +0400

    1.3 JavaDoc.Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,22 @@
 package ru.job4j.calculate;
 
+/**
+*класс для вывода привет мир
+*@autor tyuterev
+*@since 28.03.2019
+*@version 1
+*/
+
+
 public class Calculate{
+	/**
+	*main
+	*@param args - args
+	*/
 	public static void main(String[] args) {
+		/** 
+		* вывод привет мир
+		*/
 		System.out.println("Hello, World");
 	}
 }
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Nikita Tyuterev (nik353198@gmail.tu)
+* @version 1
+* @since 28.03.2019
+*/
+package ru.job4j.calculate;
